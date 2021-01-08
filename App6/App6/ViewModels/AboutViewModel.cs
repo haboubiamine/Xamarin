2097323e245bf2f7b22dc63c5d2a845becaf62be
haboubiamine <aminehaboubi00@gmail.com>
2021-01-08 06:00:24 +0100

    Add project files.

---
+++ App6/App6/ViewModels/AboutViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace App6.ViewModels
+{
+    public class AboutViewModel : BaseViewModel
+    {
+        public AboutViewModel()
+        {
+            Title = "About";
+            
+        }
+
+        public ICommand OpenWebCommand { get; }
+    }
+}
---
+++ App6/App6/ViewModels/ItemsViewModel.cs
@@ -0,0 +1,34 @@
+﻿using App6.Views;
+using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App6.ViewModels
+{
+    public class ItemsViewModel : BaseViewModel
+    {
+       
+
+        public ItemsViewModel()
+        {
+            Title = "Home";
+            
+        }
+
+       
+
+        public void OnAppearing()
+        {
+            IsBusy = true;
+           
+        }
+
+      
+
+       
+
+       
+    }
+}
